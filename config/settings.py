028ae8af43e605ba0cd1dbb8e994c7937d6c5ede
Kassem79 <sherifkassem@hotmail.com>
2026-02-23 16:30:42 +0000

    Add basic styles

--- config/settings.py
+++ config/settings.py
@@ -34,9 +34,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    '.herokuapp.com',
+    "127.0.0.1",
+    "localhost",
+    ".herokuapp.com",
 ]
 
 
